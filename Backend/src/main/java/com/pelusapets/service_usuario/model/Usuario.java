9eff32dbbd399ccc19104493033ac6843d70da1b
Bryan Guzman <bry.guzman@duocuc.cl>
2026-05-06 22:59:00 -0400

    Validaciones

--- Backend/src/main/java/com/pelusapets/service_usuario/model/Usuario.java
+++ Backend/src/main/java/com/pelusapets/service_usuario/model/Usuario.java
@@ -1,14 +1,9 @@
 package com.pelusapets.service_usuario.model;
 
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,15 +15,26 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Usuario {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-  private String nombreUsusario;
-  private String email;
-  private String password;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @ManyToOne(fetch = FetchType.EAGER)
-  @JoinColumn(name = "idRol")
-  private Rol rol;
+    @NotBlank(message = "El nombre de usuario no puede estar vacío")
+    @Size(min = 4, max = 20, message = "El nombre debe tener entre 4 y 20 caracteres")
+    @Column(nullable = false, unique = true)
+    private String nombreUsusario;
 
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "Debe ingresar un formato de email válido")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotBlank(message = "La contraseña no puede estar vacía")
+    @Size(min = 8, message = "La contraseña debe tener al menos 8 caracteres")
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "idRol", nullable = false)
+    private Rol rol;
 }
